96822fa6110dc6f4e1c8b5badf0a4ddd5b95052e
abbas-cedcoss <zaidi.yam5@gmail.com>
2022-05-07 03:07:30 +0530

    true

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
-export const isLive = false;
+export const isLive = true;
 export const CLIENT_ID = "48080de1a3764989947c53da8f20dea4"
 export const REDIRECT_URI = isLive ? "https://spotify-clone-chi-nine.vercel.app/panel/login" : "http://localhost:3000/panel/login"
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
