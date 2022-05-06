042281475b43c4f50cefdeba5199a08722c603be
abbas-cedcoss <zaidi.yam5@gmail.com>
2022-05-07 03:01:23 +0530

    islive

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,10 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, AutoComplete } from 'antd';
 import { searchsong } from './searchApi'
-
+const mockVal = (str, repeat = 1) => ({
+  value: str.repeat(repeat),
+});
 function Search() {
   const navigate = useNavigate();
+  const [value, setValue] = useState('');
+  const [options, setOptions] = useState([]);
+
+  const onSearch = (searchText) => {
+    setOptions(
+      !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)],
+    );
+  };
+
+  const onSelect = (data) => {
+    console.log('onSelect', data);
+  };
+
+  const onChange = (data) => {
+    setValue(data);
+  };
 
   async function getSong() {
     let data = await searchsong({ q: 'hate', type: 'track' });
@@ -22,10 +40,10 @@ function Search() {
   useEffect(() => {
     getSong()
     // console.log('mounted')
-  }, [])
+  }, []);
 
   return (
-    <div className=' '>
+    <div className='core-search'>
       <Form
         name="basic"
         initialValues={{
@@ -42,15 +60,15 @@ function Search() {
             },
           ]}
         >
-          <Input />
-        </Form.Item>
-
-        <Form.Item 
-        >
-          <Button type="primary" block onClick={() => {
-          }}>
-            Login
-          </Button>
+          <AutoComplete
+            options={options}
+            style={{
+              width: 200,
+            }}
+            onSelect={onSelect}
+            onSearch={onSearch}
+            placeholder="input here"
+          />
         </Form.Item>
       </Form>
     </div>
